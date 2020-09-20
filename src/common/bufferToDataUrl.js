9d713ab96e8933bf343db07af16a336cbd03ad09
Oliver Plummer <oliver.plummer@outlook.com>
2020-09-20 14:34:27 +1000

    Delete bufferToDataUrl.js

--- src/common/bufferToDataUrl.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bufferToDataUrl = (buffer) => `data:image/jpeg;base64,${buffer.toString("base64")}`;
-exports.default = bufferToDataUrl;
